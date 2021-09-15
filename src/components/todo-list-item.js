9537091d03d011634c7568311cf5a74a58fe4da5
Egidijus Bockus <egidijusbockus@gmail.com>
2021-09-15 08:14:20 +0300

    Add todo-list-item

---
+++ src/components/todo-list-item.js
@@ -0,0 +1,3 @@
+export const TodoListItem = () => {
+  return <span>Drink Coffee</span>;
+};
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,9 +1,10 @@
+import { TodoListItem } from "./todo-list-item";
+
 export const TodoList = () => {
-  const items = ["Drink coffee", "Build Awesome App"];
   return (
     <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
+      <li><TodoListItem /></li>
+      <li><TodoListItem /></li>
     </ul>
   );
 };
