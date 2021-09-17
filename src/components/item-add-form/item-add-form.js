c3a741acd1e614637468d47a205b97b9554fbc77
Egidijus Bockus <egidijusbockus@gmail.com>
2021-09-17 15:13:59 +0300

    Clear input

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,14 +4,38 @@ import "./item-add-form.css";
 export class ItemAddForm extends Component {
   constructor() {
     super();
-    this.setState = {};
+
+    this.state = {
+      label: "",
+    };
+
+    this.onLabelChange = (e) => {
+      this.setState({
+        label: e.target.value,
+      });
+    };
+
+    this.onSubmit = (e) => {
+      e.preventDefault();
+      this.props.onAdded(this.state.label);
+      this.setState({
+        label: "",
+      });
+    };
   }
 
   render() {
     return (
-      <div className="item-add-form">
-        <button className="btn btn-outline-secondary" onClick={() => this.props.onAdded("Hello")}>Add Item</button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done"
+          value={this.state.label}
+        />
+        <button className="btn btn-outline-secondary">Add Item</button>
+      </form>
     );
   }
 }
